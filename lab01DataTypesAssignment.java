44b7a9ac6c8ee90052f4d69f25e11f0bfd6e51f5
Nazer-alzaabi <nazr.uvc@gmail.com>
2026-02-23 02:58:21 +0400

    Add files via upload
    My first lab was to build a very simple program or data variables that hold a date to understand how does it works.

---
+++ lab01DataTypesAssignment.java
@@ -0,0 +1,31 @@
+public class lab01DataTypesAssignment {
+    
+    public static void main(String[] arg){
+        
+     /*First step we need to create String variable*/
+       String Username = "Nazer alzaabi";
+       
+       System.out.print("Account name : " + Username );
+       
+       int AccountNumber,AccountBalance,Savings;
+       
+       AccountNumber =  2014101590 ;
+       
+       System.out.println("Account number : " + AccountNumber);
+       
+       Savings = 17712;
+       
+       AccountBalance = 5184 ;
+       
+       double indouble = AccountBalance;
+       
+       System.out.println("Balance in Current Account : $" + indouble);
+       
+       
+       double inSavingsdouble = Savings;
+       
+       System.out.println("Balance in Current Account : $"  +  inSavingsdouble);
+      
+    }
+    
+}
